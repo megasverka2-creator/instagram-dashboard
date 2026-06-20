9ace6351c680be259409251ae678901663473f31
Muslim Safarov <muslimsafarov0324@gmail.com>
2026-06-20 22:38:10 +0500

    eddo build final

--- menyu/eddo/eddo-tv/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
-
-export default defineConfig({
-  base: './',
-  plugins: [react(), viteSingleFile()],
-})
